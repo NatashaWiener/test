d2c164cad1ba15a3671931b4335bed4cda9d619f
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-10-31 20:23:34 +0100

    add homework.java

--- src/homework.java
+++ src/homework.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class homework {
 
   public static void main(String[] args) {
     System.out.println("Hello word");
+    Scanner scanner = new Scanner(System.in);
 
+    int[] arr = new int[5];
+    for (int i = 0; i < 5; i = i + 1) {
+      arr[i] = scanner.nextInt();
+    }
+    for (int i = 0; i < 5; i = i + 1) {
+      if (arr[i] % 2 == 0) {
+        System.out.println(arr[i]);
+      }
+    }
   }
 
 }
