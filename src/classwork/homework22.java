2fd5a9e664a553ef3f116b95d4a621b4c57eb7a3
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-10-31 19:10:45 +0100

    add homework22.java

--- src/classwork/homework22.java
+++ src/classwork/homework22.java
@@ -1,2 +1,30 @@
-package classwork;public class homework22 {
+package classwork;
+
+import java.util.Scanner;
+
+public class homework22 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double sum = 0;
+    int count = 0;
+
+
+    while (true) {
+      double number = scanner.nextDouble();
+      if (number == 0) {
+        break;
+      }
+      sum += number;
+      count++;
+    }
+    if (count > 0) {
+      double average = sum / count;
+      System.out.println(average);
+    }
+
+  }
+}
+  }
+
 }
