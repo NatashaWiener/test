cef5d06a74632c53cdd4f182e0b9249fae6041f1
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-01 19:11:22 +0100

    add some code

--- src/classwork/Hello.java
+++ src/classwork/Hello.java
@@ -5,7 +5,26 @@ import java.util.Scanner;
 public class Hello {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double sum = 0;
+    int count = 0;
 
+
+    while (true) {
+      double number = scanner.nextDouble();
+      if (number == 0) {
+        break;
+      }
+      sum += number;
+      count++;
+    }
+    if (count > 0) {
+      double average = sum / count;
+      System.out.println(average);
+    }
+
+  }
+}
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
