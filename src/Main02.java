48b633ff7e1ede0ad3c8b8d6bca5e5cb4699e043
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-03 20:26:07 +0100

    add main02

--- src/Main02.java
+++ src/Main02.java
@@ -1,7 +1,7 @@
 public class Main02 {
 
   public static void main(String[] args) {
-    System.out.println("this is sumple task?");
+    System.out.println("this is simple task?");
     System.out.println("feature test");
 
   }
